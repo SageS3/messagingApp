dcf90f45ab0a427fa7a4b9b647535eb80ef2cc5a
Sage Souza <sage.souza333@gmail.com>
2024-05-07 19:11:29 -0700

    Add validator function

--- src/models/conversation.js
+++ src/models/conversation.js
@@ -1,16 +1,23 @@
 const mongoose = require('mongoose')
 // this code allows to model data 
-const conversationSchema = new mongoose.Schema({ 
-  time:time,
+const conversationSchema = new mongoose.Schema({
+  time:String,
   members:[String],
   messages:[ 
     {
       sender:String, 
       message:String, 
-      timestamp:time
+      timestamp:String
     }
   ],
   totalMessages:Number
 });
+
+// validator function limits messages array
+conversationSchema.path('messages').validate(function(messages) {
+  const limit = 20
+  return messages.length <= limit;
+}, `The number of messages exceeds the limit.`);
+
 // exporting the model allows for use across files
-module.exports = mongoose.model('Conversation', conversationSchema)
+module.exports = mongoose.model('Conversation', conversationSchema, "conversations")
