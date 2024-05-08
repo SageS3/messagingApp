ed5403ba5b00ef4b44bd06cb6f30bc668ae9a8d4
Sage Souza <sage.souza333@gmail.com>
2024-05-07 19:09:54 -0700

    Add post request

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const cors = require("cors")
 const app = express()
+const Conversation = require('./models/conversation')
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) 
@@ -22,6 +24,17 @@ app.get('/api/messages', (req, res) => {
   }
 })
 
+app.post('/api/conversations', async (req,res) => {
+  const conversation = new Conversation(req.body) 
+  try{ 
+    await conversation.save()
+    res.status(201).json(conversation)
+    console.log(conversation)
+  }catch(err){ 
+    res.status(400).json({error:err})
+  }
+})
+
 const start = async() => { 
   try{ 
     await mongoose.connect(CONNECTION)
@@ -34,3 +47,5 @@ const start = async() => {
 }
 
 start()
+
+
