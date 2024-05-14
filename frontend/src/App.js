8dadfc29f4cb33ecbc62334a6b40da5b21d979cf
Sage Souza <sage.souza333@gmail.com>
2024-05-14 13:52:32 -0700

    Add frontend folder with create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {BrowserRouter as router, Route, Switch} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router> 
+      <switch> 
+      <div className="App">
+      </div>
+      </switch>
+    </Router>
+  );
+}
+
+export default App;
