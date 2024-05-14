6c6a59696a22ddda315af510cdb564f6aac58909
Sage Souza <sage.souza333@gmail.com>
2024-05-14 14:35:24 -0700

    Add frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import {BrowserRouter as router, Route, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
-    <Router> 
-      <switch> 
       <div className="App">
       </div>
-      </switch>
-    </Router>
   );
 }
 
