5fa18d96355add75a84028d7193742c23b8f31d5
Sage Souza <sage.souza333@gmail.com>
2024-05-14 17:20:37 -0700

    Add react router dom to add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
-
+import { Link, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Conversations } from "./pages/Conversations"
+import "./App.css"
 
 function App() {
   return (
-      <div className="App">
-      </div>
-  );
+    <div className="App">
+      <nav>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/conversations">Threads</Link>
+        </button>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/conversations" element={<Conversations />} />
+      </Routes>
+    </div>
+  )
 }
 
-export default App;
+export default App
