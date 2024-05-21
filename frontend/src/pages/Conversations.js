b165d8e100684e8bf4bcaef04bf652fdddf6f770
Sage Souza <sage.souza333@gmail.com>
2024-05-21 10:38:21 -0700

    Add block of code that fetches conversations from axios, maped conversations and conditionally render them

--- frontend/src/pages/Conversations.js
+++ frontend/src/pages/Conversations.js
@@ -1,3 +1,43 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import axios from "axios"
 export function Conversations() {
-  return <div>conversations</div>
+  const [conversations, setConversations] = useState(null)
+  const [getDataError, setGetDataError] = useState(undefined)
+
+  async function getConversations() {
+    try {
+      const res = await axios.get("http://localhost:3001/api/conversations")
+      setConversations(res.data.conversations)
+    } catch (error) {
+      setGetDataError(error.message)
+    }
+  }
+
+  function mapConversations() {
+    return (
+      <>
+        {conversations.map((conversation, index) => (
+          <div key={index}>
+            <p>{conversation.time}</p>
+            <button>
+              <Link to={`/conversations/${conversation._id}`}>View</Link>
+            </button>
+          </div>
+        ))}
+      </>
+    )
+  }
+
+  useEffect(() => {
+    getConversations()
+  }, [])
+
+  return (
+    <div>
+      <div>
+        {!conversations ? <div>{getDataError}</div> : mapConversations()}
+      </div>
+    </div>
+  )
 }
