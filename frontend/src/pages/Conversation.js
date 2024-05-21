6c29282416f2a4b85ff9e156c47e8c194358dd4f
Sage Souza <sage.souza333@gmail.com>
2024-05-21 16:03:09 -0700

    Add axious get request that handles errors and conditionally render messages

--- frontend/src/pages/Conversation.js
+++ frontend/src/pages/Conversation.js
@@ -1,7 +1,56 @@
 import React, { useEffect, useState } from "react"
-
+import { useParams } from "react-router-dom"
+import axios from "axios"
 export function Conversation() {
-  const [messages, setMessages] = useState([])
+  const [conversation, setConversation] = useState(null)
+  const [error, setError] = useState(null)
+  const [inputValue, setInputValue] = useState("")
+  const { conversationId } = useParams()
 
-  return <div>hello</div>
+  async function getMessages() {
+    try {
+      const conversation = await axios.get(
+        `http://localhost:3001/api/conversations/${conversationId}`
+      )
+      setConversation(conversation.data)
+    } catch (error) {
+      console.log(error)
+      switch (error.code) {
+        case "ERR_BAD_REQUEST":
+          setError("Bad Request")
+          break
+        default:
+          setError("something went wrong")
+      }
+    }
+  }
+
+  const mapMessages = () => {
+    return conversation.messages.map((message, index) => (
+      <div key={index}>{message.message}</div>
+    ))
+  }
+
+  const inputChangeHandler = (event) => {
+    setInputValue(event.target.value)
+  }
+
+  useEffect(() => {
+    getMessages()
+    return () => {
+      getMessages()
+    }
+  }, [conversationId])
+
+  return (
+    <div>
+      {!conversation ? <div>{error}</div> : mapMessages()}
+      <input
+        placeholder="type message"
+        type="text"
+        value={inputValue}
+        onChange={inputChangeHandler}
+      ></input>
+    </div>
+  )
 }
