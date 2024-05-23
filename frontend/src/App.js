644f2cea7881877161f2dc5f775d88927f5411c0
Sage Souza <sage.souza333@gmail.com>
2024-05-23 10:36:37 -0700

    Delete getConversations function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,9 @@ import { Link, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Conversations } from "./pages/Conversations"
 import { Conversation } from "./pages/Conversation"
-import axios from "axios"
 import "./App.css"
 
 function App() {
-  const getConversations = () => {
-    axios
-      .get("http://localhost:3000/api/conversations")
-      .then((res) => {
-        console.log(res)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-  getConversations()
   return (
     <div className="App">
       <nav>
@@ -30,7 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/conversations" element={<Conversations />} />
-        <Route path="/conversations/:id/" element={<Conversation />} />
+        <Route
+          path="/conversations/:conversationId/"
+          element={<Conversation />}
+        />
       </Routes>
     </div>
   )
